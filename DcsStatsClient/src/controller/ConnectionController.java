66968ad56eefaf966c1ff9b0aba3abf6c572e60d
Ked <Kedthunder@gmail.com>
2017-12-06 00:28:35 +0100

    Basic interface
    Still need the options menu

--- DcsStatsClient/src/controller/ConnectionController.java
+++ DcsStatsClient/src/controller/ConnectionController.java
@@ -1,5 +1,18 @@
 package controller;
 
+import javafx.fxml.FXML;
+
 public class ConnectionController {
 
+	private RootController rootController;
+	
+	
+	public void init(RootController rootController){
+		this.rootController = rootController;
+	}
+	
+	@FXML
+	public void connectionButtonOnAction(){		
+		rootController.connectionCallBack();
+	}
 }
